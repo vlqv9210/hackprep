d37c1892a1c072676e048f662c3f9210b8052616
Eric Pham <ericphm28@gmail.com>
2025-02-01 17:06:11 -0800

    update backend

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,40 @@
 # Create, Read, Update, Delete (CRUD) operations for the API
 from flask import Blueprint, current_app, request, jsonify, send_from_directory
 from config import app, db
-from models import User
+# from models import User
+import requests
 import os
 
 
 @app.route('/', methods=["GET"])
 def home():
     return jsonify({"message": "WORK!!!"}),200
+
+# @app.route('/', methods=["POST"])
+# def UserData():
+
+#     api_key = '5OowENhf0gUjDqxmLGf9YA'
+#     headers = {'Authorization': 'Bearer ' + api_key}
+#     api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+#     linkedin_profile_url = 'https://www.linkedin.com/in/williamhgates'
+
+#     response = requests.get(api_endpoint,
+#                         params={'url': linkedin_profile_url, 
+#                                 },
+#                         headers=headers)
+#     result = response.json()
+#     return result
+
+
+
+@app.route('/linkedinProfile', methods=["POST"])
+def UserData():
+
+    
+
+
+    return jsonify({"message": "WORK!!!"})
+
 
 
 # if run file then execute the code in this file only
--- backend/models.py
+++ backend/models.py
@@ -1,25 +1,58 @@
-from flask import request
-from sqlalchemy.orm import sessionmaker
-from config import db
+# from flask import request
+# from sqlalchemy.orm import sessionmaker
+# from config import db
 
-class User(db.Model):
-    __tablename__ = 'user'
-    username = db.Column(db.String(80), primary_key=True, nullable=False, unique=True)
-    age = db.Column(db.Integer, nullable=True)
-    skill = db.Column(db.String(80), nullable=False)
-    areaWork = db.Column(db.String(80), nullable=False)
+# # deepseek api key
+# # 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
 
-    def __init__(self, username, skill, areaWork, age=None):
-        self.username = username
-        self.age = age
-        self.skill = skill
-        self.areaWork = areaWork
+# class User(db.Model):
+#     __tablename__ = 'user'
+#     username = db.Column(db.String(80), primary_key=True, nullable=False, unique=True)
+#     jobTitle = db.Column(db.String(80), nullable=False, unique=True)
+#     city = db.Column(db.String(80), nullable=False, unique=True)
+#     education = db.Column(db.String(80), nullable=False, unique=True)
+#     skills = db.relationship('UserSkill', backref='user', lazy=True)
 
-    def to_json(self):
-        return {
-            "username": self.username,
-            "age": self.age,
-            "skill": self.skill,
-            "areaWork" : self.areaWork
 
-        }
+#     def __init__(self, username, jobTitle, city, education, skills):
+#         self.username = username
+#         self.jobTitle = jobTitle
+#         self.city = city
+#         self.education = education
+#         self.skills = skills
+
+#     def to_json(self):
+#         return {
+            
+
+#         }
+
+
+# class UserSkill(db.Model):
+#     __tablename__ = 'skill'
+#     id = db.Column(db.Integer, primary_key=True)
+#     # ondelete='CASCADE' means that if a Pokemon/Skill is deleted, 
+#     # this instance will also be deleted.
+#     # user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     skill_id = db.Column(db.Integer, db.ForeignKey('skill.id'), nullable=False)
+
+
+# class Skill(db.Model):
+#     __tablename__ = 'skill'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), nullable=False)
+
+#     def __init__(self, id, name):
+#         self.id = id
+#         self.name = name
+
+#     def to_json(self):
+#         return {
+#             "id": self.id,
+#             "name": self.name,
+#         }
+
+    
+
+
